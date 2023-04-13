f1d920dac4862f472152f4ae234e3ce79ba1e7b9
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-04-13 19:35:12 +0500

    Создали метод для помещения записи, в нашу таблицу нашей базы данных.

--- app/src/main/java/Data/DatabaseHandler.java
+++ app/src/main/java/Data/DatabaseHandler.java
@@ -1,11 +1,13 @@
 package Data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import Model.Car;
 import Utils.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -28,5 +30,19 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + Util.TABLE_NAME);
         onCreate(db);
+    }
+
+    // CRUD - Create, Read, Update, Delete
+
+    public void addCar(Car car) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Util.KEY_NAME, car.getName());
+        contentValues.put(Util.KEY_PRICE, car.getPrice());
+        // мы не вставляем KEY_ID, т.к. он добавляется автоматически
+
+        db.insert(Util.TABLE_NAME, null, contentValues);
+        db.close();
     }
 }
